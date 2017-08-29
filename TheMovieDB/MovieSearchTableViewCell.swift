9fcd48fbe4785ad4f0033bebcd1fd1d323c60dda
Mateo Echeverri Yepes <mateo.echeverri@globant.com>
2017-08-29 13:10:48 -0500

    started implementing

---
+++ TheMovieDB/MovieSearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  TheMovieDB
+//
+//  Created by Mateo Echeverri Yepes on 8/28/17.
+//  Copyright © 2017 Globant. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
